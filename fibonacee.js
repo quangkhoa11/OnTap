7a2d7f254e1d4ef5478e7bd9e46fda7475308f77
quangkhoa11 <khoa110303@gmail.com>
2025-09-23 14:33:18 +0700

    fibonacee.js

--- fibonacee.js
+++ fibonacee.js
@@ -1,3 +1,4 @@
+//Dùng For
 function fibonacci(n){
     let soDau = 0;
     let soHai = 1;
@@ -19,3 +20,19 @@ function fibonacci(n){
     return soHai;
 }
 console.log("Kết quả phân tử 9: ",fibonacci(9))
+
+//Dùng đệ quy
+function fibonacciDQ(num){
+    let a = 0;
+    let b = 1;
+    if(num===0){
+        return a;
+    }
+    if(num===1){
+        return b;
+    }
+    if(num>=2){
+        return fibonacciDQ(num - 1) + fibonacciDQ(num - 2);
+    }
+}
+console.log(fibonacciDQ(9));
